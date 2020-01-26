c3bd717695b89862408f34bcdddfe0a4eed70b75
desi9n test <test@desi9n.pl>
2020-01-26 17:15:28 +0100

    You now exact when to add ingredients to soup now.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 $.getJSON('table.json', function(data){
 
-    const buttons = document.querySelectorAll('button'); //node.list zawierająca wszystkie przyciski
+    const buttons = document.querySelectorAll('button'); //node.list zawierająca wszystkie przyciski warzyw
     const ingredients = []; //tablica wszystkich wybranych składników
+
+    const now = new Date(); //pobranie czasu systemowego
+    let hour = now.getHours(); //wyciągnięcie godzin z czasu
+    let minute = now.getMinutes(); //wyciągnięcie minut z czasu
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function table(){
-            ingredients.push(data.vegetables[i].vegetable); //dodawanie klikniętych elementów do tablicy
+            ingredients.push(data.components[i].component); //dodawanie klikniętych elementów do tablicy
             ingredients.sort(function(a, b) { //posortowanie tablicy
                 return a.value - b.value;
             });
@@ -14,9 +18,15 @@ $.getJSON('table.json', function(data){
     }
 
     document.querySelector('.result').addEventListener('click', function(){//funkcja pokazująca wynik w konsoli
-        console.log(`Najsampierw wrzuć ${ingredients[0].name}`);
+        console.log(`Najsampierw wrzuć ${ingredients[0].name} o ${hour}:${minute}`);
         for (let i = 1; i < (ingredients.length); i++) {
-            console.log(`po ${ingredients[0].value - ingredients[i].value} minutach od rozpoczęcia gotowania wrzuć ${ingredients[i].name}`);
+            const addingMinutes = ingredients[0].value - ingredients[i].value; //po jakim czasie od rozpoczęcia gotowania wrzucić
+            let addingTime = minute + addingMinutes;
+            if(addingTime>59){
+                addingTime = addingTime - 60;
+                hour +=1;
+            }
+            console.log(`o ${hour} : ${addingTime} wrzuć ${ingredients[i].name}`);
         }
     })
 
