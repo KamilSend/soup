0d4356bd7f31fe0bfbce229653dc6dd53f772448
desi9n test <kam.sendecki@gmail.com>
2020-03-16 11:54:40 +0100

    fix - before, some cancelled products still appear in result, but active one sometimes didn't

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $.getJSON('table.json', function(data){
 
     const buttons = document.querySelectorAll('button'); //node.list zawierająca wszystkie przyciski składników
-    const ingredients = []; //tablica wszystkich wybranych składników
+    const ingredients = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]; //tablica wszystkich wybranych składników
     const nowButton = document.querySelector('.now');
     const display = document.querySelector('#displayResult');
 
@@ -42,31 +42,68 @@ $.getJSON('table.json', function(data){
 
 
    //wybieranie składników-------------------------------------------------------------------------
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function table(){
-            buttons[i].classList.toggle('btnActive');//zaznaczenie wybranego składnika
-            if (buttons[i].classList.contains('btnActive')){
-                ingredients.push(data.components[i].component); //dodawanie klikniętych elementów do tablicy
-                ingredients.sort(function(a, b) { //posortowanie tablicy
-                    return a.value - b.value;
-                });
-                ingredients.reverse(); //odwrócenie tablicy żeby pierwsze były największe wartości
-            }
-            else{
-                ingredients.pop(data.components[i].component);
-            }
-        })
-    }
+   //  for (let i = 0; i < buttons.length; i++) {
+   //      buttons[i].addEventListener('click', function table(){
+   //          console.log(ingredients);
+   //          buttons[i].classList.toggle('btnActive');//zaznaczenie wybranego składnika
+   //          if (buttons[i].classList.contains('btnActive')){
+   //              ingredients.push(data.components[i].component); //dodawanie klikniętych elementów do tablicy
+   //              ingredients.sort(function(a, b) { //posortowanie tablicy
+   //                  return a.value - b.value;
+   //              });
+   //              ingredients.reverse(); //odwrócenie tablicy żeby pierwsze były największe wartości
+   //          }
+   //          else{
+   //              ingredients.pop(data.components[i].component);
+   //          }
+   //      })
+   //  }
+
+     for (let i = 0; i < buttons.length; i++) {
+         buttons[i].addEventListener('click', function table(){
+             //console.log(ingredients);
+             buttons[i].classList.toggle('btnActive');//zaznaczenie wybranego składnika
+             if (buttons[i].classList.contains('btnActive')){
+                 ingredients[i]=data.components[i].component;
+                 //ingredients.push(data.components[i].component); //dodawanie klikniętych elementów do tablicy
+             }
+             else{
+                 ingredients.splice(i, 1,0);
+                 //console.log(ingredients);
+             }
+         })
+     }
 
     //obliczanie czasów dodawania składników--------------------------------------------------------
     document.querySelector('.result').addEventListener('click', function(){
+        //console.log(ingredients);
+        let helpingTable = ingredients.slice();
+        //const finalIngredients = [];
 
-        if(ingredients.length>1){
-            //display.textContent +=`Najpierw wrzuć ${ingredients[0].name} o ${hour}:${minute}`;
-            display.innerHTML +=`First add ${ingredients[0].name} at ${hour}:${minute}`;
-            for (let i = 1; i < (ingredients.length); i++) {
+        for (let i = (ingredients.length-1); i >=0; i--) {
+            if (ingredients[i] === 0){
+                helpingTable.splice(i,1);
 
-                const addingMinutes = ingredients[0].value - ingredients[i].value; //po jakim czasie od rozpoczęcia gotowania wrzucić
+                //console.log(ingredients);
+                //console.log(helpingTable);
+            }
+        }
+
+        function compareScore(a, b) {
+            return a.value - b.value;
+        }
+
+        helpingTable.sort(compareScore);
+
+        helpingTable.reverse();
+        console.log(ingredients);
+        console.log(helpingTable);
+
+            if(helpingTable.length>1){
+            display.innerHTML +=`First add ${helpingTable[0].name} at ${hour}:${minute}`;
+            for (let i = 1; i < (helpingTable.length); i++) {
+                if(helpingTable[i]!==0){
+                const addingMinutes = helpingTable[0].value - helpingTable[i].value; //po jakim czasie od rozpoczęcia gotowania wrzucić
                 let addingTime = minute + addingMinutes;
                 let addingHour = hour;
                 //zadziała tylko jeśli nie trzeba będzie przerzucać godziny więcej niż raz, czyli wywali się przy różnicach większych od godziny
@@ -80,11 +117,11 @@ $.getJSON('table.json', function(data){
 
                 const el = [];//utworzenie tablicy elementów div w których będą wyświetlane kolejne czasy
                 el[i] =document.createElement("div");
-                el[i].textContent = ` at ${addingHour} : ${addingTime} add ${ingredients[i].name}`;
+                el[i].textContent = ` at ${addingHour} : ${addingTime} add ${helpingTable[i].name}`;
                 display.appendChild(el[i]);
                 display.classList.add('active');
                 document.getElementById('cover').classList.add('active');
-                el[i] = '';
+                el[i] = '';}
 
                 document.querySelector('.close').addEventListener('click', function(){
                     display.classList.remove('active');
@@ -94,19 +131,21 @@ $.getJSON('table.json', function(data){
 
             }
         }
-        else if (ingredients.length==1){
-            display.innerHTML +=`First add ${ingredients[0].name} at ${hour}:${minute}`;
-            display.classList.add('active');
-            document.getElementById('cover').classList.add('active');
-
-            document.querySelector('.close').addEventListener('click', function(){
-                display.classList.remove('active');
-                document.querySelector('#cover').classList.remove('active');
-                display.innerHTML ='<div id="close" class="close">X</div>';
-            });
-        }
+        // else if (ingredients.length==1){
+        //     console.log(ingredients);
+        //     display.innerHTML +=`First add ${ingredients[0].name} at ${hour}:${minute}`;
+        //     display.classList.add('active');
+        //     document.getElementById('cover').classList.add('active');
+        //
+        //     document.querySelector('.close').addEventListener('click', function(){
+        //         display.classList.remove('active');
+        //         document.querySelector('#cover').classList.remove('active');
+        //         display.innerHTML ='<div id="close" class="close">X</div>';
+        //     });
+        // }
     });
 
 
 });
+
 
